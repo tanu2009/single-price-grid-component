6ff90a3bcd9a9ba69d14b4421fef1135fc755eed
tanu2009 <69074126+tanu2009@users.noreply.github.com>
2020-09-30 10:14:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("open").addEventListener("click",
+function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+
+document.getElementById("close").addEventListener("click",
+function(){
+    document.querySelector(".popup").style.display = "none";
+});
